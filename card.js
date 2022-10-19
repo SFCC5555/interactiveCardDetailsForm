b016094313470e3d0d2e4518418f8ef968a5e81d
sfcc5555 <sfcc.5555@gmail.com>
2022-10-19 00:01:35 -0500

    Actualización

--- card.js
+++ card.js
@@ -118,7 +118,7 @@ function test(evento2) {
 
 /*Card Number*/
 
-charNumberList=["1","2","3","4","5","6","7","8","9","0"," "];
+charNumberList=[1,2,3,4,5,6,7,8,9,0,"1","2","3","4","5","6","7","8","9","0"," "];
 
 let renderNumberStatus="on"
 
